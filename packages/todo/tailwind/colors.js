6c92940c5cc96550bbdb9f74322f0eb61f497e18
esme.jo <esme.jo@kakaoent.com>
2021-11-18 11:55:30 +0900

    tailwind config에서 colors 별도로 분리 (#13)

---
+++ packages/todo/tailwind/colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  black: '#000',
+  black2: '#222',
+  blue: '#4095ff',
+  gold: '#c6a66d',
+  grey01: '#ddd',
+  grey02: '#a6a6a6',
+  purple: '#6853e7',
+  red: '#ff3042',
+  transparent: 'transparent',
+  white: '#fff',
+};
