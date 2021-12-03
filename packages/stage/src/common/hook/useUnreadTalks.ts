553c9a241bcce30e3085c3fef24e354fd50bafd3
coze-nutmott <coze.nutmott@kakaoet.com>
2021-12-03 12:26:44 +0900

    resolve ts error (Todo: temporal ignore)

--- packages/stage/src/common/hook/useUnreadTalks.ts
+++ packages/stage/src/common/hook/useUnreadTalks.ts
@@ -1,3 +1,6 @@
+// @ts-nocheck
+// TODO: Coze: unlock nocheck option later
+
 import { queryFn } from 'common/util/api';
 import { useQuery } from 'react-query';
 import useSession from './useSession';
