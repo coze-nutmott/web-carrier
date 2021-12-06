14fbfbec79a3b97de3e952606baf132073cd0d6a
coze-nutmott <coze.nutmott@kakaoet.com>
2021-12-06 14:34:08 +0900

    fix tailwind typo

--- packages/stage/src/nickname/component/Modal/NewNicknameModal.tsx
+++ packages/stage/src/nickname/component/Modal/NewNicknameModal.tsx
@@ -25,7 +25,7 @@ export default function NewNicknameModal({ close, onInputNickname }: IProps) {
       <Flex as="form" onSubmit={handleSubmit(handleSubmitNickname)}>
         <Column className="px-24 pt-24 pb-32 w-full">
           <Text className="text-16 font-medium">새 닉네임 추가</Text>
-          <Text className="mt-16 mb-8 text-14 font-normal font-black">
+          <Text className="mt-16 mb-8 text-14 font-normal text-black">
             닉네임
           </Text>
           <Flex className="rounded-2 border-1 border-solid border-diviceLine">
